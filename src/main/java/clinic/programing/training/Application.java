3f76874fb104b473ff4f0cd8cd4ae7f1cbc9f12d
spoiler335 <54944126+spoiler335@users.noreply.github.com>
2021-03-25 17:35:32 +0530

    Added Dependencies to lang3

--- src/main/java/clinic/programing/training/Application.java
+++ src/main/java/clinic/programing/training/Application.java
@@ -1,9 +1,17 @@
 package clinic.programming.training;
 
 import java.util.*;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application
 {
+
+    public int countWords(String words)
+    {
+        String[] separateWords = StringUtils.split(words, ' ');
+        return (separateWords==null)?0:separateWords.length;
+    }
+
     public void greet()
     {
         List<String> greetings= new ArrayList();
@@ -25,5 +33,7 @@ public class Application
         System.out.println("Starting Application");
         Application app=new Application();
         app.greet();
+        int count=app.countWords("I have four words");
+        System.out.println("Word Count: "+count);
     }
 }
